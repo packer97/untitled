3c4327830be11ebe8314211c0dbeab85b93dbb4c
packer97 <serovstanislav@yandex.ru>
2021-04-20 18:43:38 +0400

    first commit

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,10 @@
 public class Class {
+    public static void main(String[] args) {
+        int f = 15;
+        int nums[] = new int[34];
+        for (int i = 0; i <= nums.length - 1; i++) {
+            nums[i] = i;
+            System.out.println(nums[i]);
+        }
+    }
 }
